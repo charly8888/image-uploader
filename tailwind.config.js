747fa660fc8adfa0696d00c28055a9eb05408487
German Hornus <hornusgerman@gmail.com>
2022-06-08 10:10:21 -0300

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html','./**/*.js'],
   theme: {
     extend: {
       fontFamily: {
